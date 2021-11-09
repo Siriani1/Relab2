d73d95f64ff10b54c16f7d4a4f1bf90af4c87e8a
Sirio <siriani.andrea@iisgalvanimi.edu.it>
2021-11-09 13:10:30 +0100

    RelabClient

--- RelabClient/src/app/app.component.ts
+++ RelabClient/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps'
+import { Observable } from 'rxjs';
+import {  GeoFeatureCollection } from './models/geojson.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'RelabClient';
+export class AppComponent implements AfterViewInit {
+  title = 'server mappe';
+  //Variabile che conterrà i nostri oggetti GeoJson
+  geoJsonObject : GeoFeatureCollection;
+  //Observable per richiedere al server python i dati sul DB
+  obsGeoData: Observable<GeoFeatureCollection>;
+  // Centriamo la mappa
+  center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
+  zoom = 8;
+
+  constructor(public http: HttpClient) {
+    //Facciamo iniettare il modulo HttpClient dal framework Angular (ricordati di importare la libreria)
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  //Metodo che scarica i dati nella variabile geoJsonObject
+  prepareData = (data: GeoFeatureCollection) => {
+    this.geoJsonObject = data
+    console.log( this.geoJsonObject );
+  }
+
+ //Una volta che la pagina web è caricata, viene lanciato il metodo ngOnInit scarico i dati 
+  //dal server
+  ngOnInit() { 
+    this.obsGeoData = this.http.get<GeoFeatureCollection>("mongodb+srv://sirio:WlLryCFHOXnBKMbx@cluster0.jlysa.mongodb.net/Relab?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE/ci_vettore/50");
+    this.obsGeoData.subscribe(this.prepareData);
+  }
 }
